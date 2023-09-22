a8c8239a7b78384d22c8fd2f8400d927f1253823
Raj <raj.chaudhary@ebpearls.com>
2023-09-22 07:24:44 +0545

    setting up

--- src/routes/appRoutes.tsx
+++ src/routes/appRoutes.tsx
@@ -7,11 +7,11 @@ import ChangelogPage from "../pages/changelog/ChangelogPage";
 import AnalyticsPage from "../pages/dashboard/AnalyticsPage";
 import SaasPage from "../pages/dashboard/SaasPage";
 import ComponentPageLayout from "../pages/component/ComponentPageLayout";
-import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
-import AppsOutlinedIcon from '@mui/icons-material/AppsOutlined';
-import ArticleOutlinedIcon from '@mui/icons-material/ArticleOutlined';
-import FormatListBulletedOutlinedIcon from '@mui/icons-material/FormatListBulletedOutlined';
-import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
+import DashboardOutlinedIcon from "@mui/icons-material/DashboardOutlined";
+import AppsOutlinedIcon from "@mui/icons-material/AppsOutlined";
+import ArticleOutlinedIcon from "@mui/icons-material/ArticleOutlined";
+import FormatListBulletedOutlinedIcon from "@mui/icons-material/FormatListBulletedOutlined";
+import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
 import AlertPage from "../pages/component/AlertPage";
 import ButtonPage from "../pages/component/ButtonPage";
 import InstallationPage from "../pages/installation/InstallationPage";
@@ -21,37 +21,37 @@ const appRoutes: RouteType[] = [
   {
     index: true,
     element: <HomePage />,
-    state: "home"
+    state: "home",
   },
   {
     path: "/installation",
     element: <InstallationPage />,
     state: "installation",
     sidebarProps: {
-      displayText: "Installation",
-      icon: <FileDownloadOutlinedIcon />
-    }
+      displayText: "Typography",
+      icon: <FileDownloadOutlinedIcon />,
+    },
   },
   {
     path: "/dashboard",
     element: <DashboardPageLayout />,
     state: "dashboard",
     sidebarProps: {
-      displayText: "Dashboard",
-      icon: <DashboardOutlinedIcon />
+      displayText: "Components",
+      icon: <DashboardOutlinedIcon />,
     },
     child: [
       {
         index: true,
         element: <DashboardIndex />,
-        state: "dashboard.index"
+        state: "dashboard.index",
       },
       {
         path: "/dashboard/default",
         element: <DefaultPage />,
         state: "dashboard.default",
         sidebarProps: {
-          displayText: "Default"
+          displayText: "Button",
         },
       },
       {
@@ -59,64 +59,64 @@ const appRoutes: RouteType[] = [
         element: <AnalyticsPage />,
         state: "dashboard.analytics",
         sidebarProps: {
-          displayText: "Analytic"
-        }
+          displayText: "Chips",
+        },
       },
       {
         path: "/dashboard/saas",
         element: <SaasPage />,
         state: "dashboard.saas",
         sidebarProps: {
-          displayText: "Saas"
-        }
-      }
-    ]
-  },
-  {
-    path: "/component",
-    element: <ComponentPageLayout />,
-    state: "component",
-    sidebarProps: {
-      displayText: "Components",
-      icon: <AppsOutlinedIcon />
-    },
-    child: [
-      {
-        path: "/component/alert",
-        element: <AlertPage />,
-        state: "component.alert",
-        sidebarProps: {
-          displayText: "Alert"
+          displayText: "Breadcrumb",
         },
       },
-      {
-        path: "/component/button",
-        element: <ButtonPage />,
-        state: "component.button",
-        sidebarProps: {
-          displayText: "Button"
-        }
-      }
-    ]
+    ],
   },
-  {
-    path: "/documentation",
-    element: <DocumentationPage />,
-    state: "documentation",
-    sidebarProps: {
-      displayText: "Documentation",
-      icon: <ArticleOutlinedIcon />
-    }
-  },
-  {
-    path: "/changelog",
-    element: <ChangelogPage />,
-    state: "changelog",
-    sidebarProps: {
-      displayText: "Changelog",
-      icon: <FormatListBulletedOutlinedIcon />
-    }
-  }
+  // {
+  //   path: "/component",
+  //   element: <ComponentPageLayout />,
+  //   state: "component",
+  //   sidebarProps: {
+  //     displayText: "Components",
+  //     icon: <AppsOutlinedIcon />,
+  //   },
+  //   child: [
+  //     {
+  //       path: "/component/alert",
+  //       element: <AlertPage />,
+  //       state: "component.alert",
+  //       sidebarProps: {
+  //         displayText: "Alert",
+  //       },
+  //     },
+  //     {
+  //       path: "/component/button",
+  //       element: <ButtonPage />,
+  //       state: "component.button",
+  //       sidebarProps: {
+  //         displayText: "Button",
+  //       },
+  //     },
+  //   ],
+  // },
+  // {
+  //   path: "/documentation",
+  //   element: <DocumentationPage />,
+  //   state: "documentation",
+  //   sidebarProps: {
+  //     displayText: "Documentation",
+  //     icon: <ArticleOutlinedIcon />,
+  //   },
+  // },
+  // {
+  //   path: "/changelog",
+  //   element: <ChangelogPage />,
+  //   state: "changelog",
+  //   sidebarProps: {
+  //     displayText: "Changelog",
+  //     icon: <FormatListBulletedOutlinedIcon />,
+  //   },
+  // },
 ];
 
 export default appRoutes;
