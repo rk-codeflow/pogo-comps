1af1b1d1ecb6122a980df0df9b37f9764b419f84
Raj <raj.chaudhary@ebpearls.com>
2023-09-22 07:20:35 +0545

    project initials

---
+++ src/pages/component/AlertPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const AlertPage = (props: Props) => {
+  return <div>I am an Chip</div>;
+};
+
+export default AlertPage;
---
+++ src/pages/installation/InstallationPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const InstallationPage = (props: Props) => {
+  return <div>InstallationPage</div>;
+};
+
+export default InstallationPage;
